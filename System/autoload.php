b439aa8b3a2343a10f7ccb5ac266756b8c6c0177
HRVB <h.boulangue@gmail.com>
2019-02-13 09:59:16 +0100

    Change str_replace content (autoload.php) / #11

--- System/autoload.php
+++ System/autoload.php
@@ -5,7 +5,7 @@ spl_autoload_register(
     */
     function($class)
     {
-        $class = str_replace('Controllers\\', 'Controllers/', $class);
+        $class = str_replace("\\", "/", $class);
         include_once("../" . $class . ".php");
     }
 );
